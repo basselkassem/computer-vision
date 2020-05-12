97c978cdc895c54203ac2eae1bcbd01546a11bf1
Bassel KASSEM <bassel.sh.k@gmail.com>
2020-05-12 19:49:03 +0200

    Update ops.py

--- ops.py
+++ ops.py
@@ -4,9 +4,7 @@
 
 import math
 import numpy as np
-#import tensorflow as tf
-import tensorflow.compat.v1 as tf
-tf.disable_v2_behavior()
+import tensorflow as tf
 from utils import *
 
 class batch_norm(object):
